f2755053354feccac30cce0f6005f99051d838c4
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-06-01 21:33:20 +0200

    1

--- errorCom.js
+++ errorCom.js
@@ -43,17 +43,17 @@ class ErrorCom {
     el.style.transition = "all 2s cubic-bezier(0.82, 0.02, 0.36, 1.02)";
 
     el.style.opacity = "0";
-    el.style.transform = "translate(0, 20px)";
+    el.style.transform = `translate(${this.left}, calc(${this.top}) + 20px)`;
     this.ids.push(setTimeout(() => {
       el.style.opacity = "1";
-      el.style.transform = "translate(0, 0)";
+      el.style.transform = `translate(${this.left}, calc(${this.top}) + 0)`;
     }, 100))
     this.ids.push(setTimeout(() => {
-      el.style.transform = "translate(0, 0)";
+      el.style.transform = `translate(${this.left}, calc(${this.top}) + 0)`;
     }, 4000));
     this.ids.push(setTimeout(() => {
       el.style.opacity = "0";
-      el.style.transform = "translate(0, 20px)";
+      el.style.transform = `translate(${this.left}, calc(${this.top}) + 20px)`;
       this.running = false;
     }, 4400));
   };
